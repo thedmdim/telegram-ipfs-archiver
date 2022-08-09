6e7afdfe4415dd282473df3b77c5eaf808067010
thedmdim <thedmdim@gmail.com>
2022-08-09 23:04:13 +0400

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ipfs init
 key=$(find . -name '*.key' -print -quit)
-if $key; then ipfs key import key $key; fi
+if [ "$key" ]; then ipfs key import key $key; fi
 ipfs daemon & python bot.py
