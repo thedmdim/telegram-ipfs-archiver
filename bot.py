121280b8ff929bfbdc0f9011a8e2b41cbf6049b3
thedmdim <thedmdim@gmail.com>
2022-08-10 14:18:53 +0400

    ipfs add on startup

--- bot.py
+++ bot.py
@@ -49,16 +49,19 @@ async def startup(dispatcher):
             else:
                 logger.info(ipfs_keys_stderr)
                 return
-        else:
-            keys = ipfs_keys.stdout.decode("utf-8").rstrip().split("\n")
-            if len(keys) > 1:
-                try:
-                    global K51_KEY
-                    K51_KEY = keys[keys.index("key")]
-                    logger.info("found a key")
-                except ValueError:
-                    logger.info("no keys found, using default")
-            return
+
+    keys = ipfs_keys.stdout.decode("utf-8").rstrip().split("\n")
+    if len(keys) > 1:
+        try:
+            global K51_KEY
+            K51_KEY = keys[keys.index("key")]
+            logger.info("found a key")
+        except ValueError:
+            logger.info("no keys found, using default")
+
+    if os.listdir(STORAGE):
+        add_and_publish()
+
 
 def download_video(link: str, retries: int):
     logger.info("Download video")
@@ -73,6 +76,35 @@ def download_video(link: str, retries: int):
             continue
         return title
     logger.error(f"Couldn't download video in {retries} tries")
+
+def add_and_publish():
+    ipfs_add = subprocess.run(["ipfs", "add", "--nocopy", "-r", f"{STORAGE}"], capture_output=True)
+
+    if ipfs_add.returncode:
+        logger.error(ipfs_add.stderr.decode("utf-8"))
+        return
+    logger.info("Folder updated")
+
+    ipfs_stdout = ipfs_add.stdout.decode("utf-8").rstrip().replace("added ", "")
+    files = [file.split(" ", 1) for file in ipfs_stdout.split("\n")]
+    files = [{"hash": i[0], "filename": i[1]} for i in files]
+
+    [dir_hash] = [i["hash"] for i in files if i["filename"] == STORAGE_DIR]
+
+    command = ["ipfs", "name", "publish", "--quieter", f"{dir_hash}"]
+    if K51_KEY:
+        logger.info(f"k51 key {K51_KEY}")
+        command.append(f"--key={K51_KEY}")
+
+    ipfs_publish = subprocess.run(command, capture_output=True)
+
+    if ipfs_publish.returncode:
+        logger.error(ipfs_publish.stderr.decode("utf-8"))
+        return
+
+    logger.info(f"IPNS updated: {ipfs_publish.stdout.decode('utf-8').rstrip()}")
+
+    return files
 
 
 @dp.channel_post_handler()
@@ -90,35 +122,13 @@ async def post(message: types.Message):
         else:
             return
 
-        ipfs_add = subprocess.run(["ipfs", "add", "--nocopy", "-r", f"{STORAGE}"], capture_output=True)
+        files = add_and_publish()
 
-        if ipfs_add.returncode:
-            logger.error(ipfs_add.stderr.decode("utf-8"))
-            return
-        logger.info("the video added to ipfs")
-
-        ipfs_stdout = ipfs_add.stdout.decode("utf-8").rstrip().replace("added ", "")
-        files = [file.split(" ", 1) for file in ipfs_stdout.split("\n")]
-        files = [{"hash":i[0], "filename":i[1]} for i in files]
-
-        [dir_hash] = [i["hash"] for i in files if i["filename"] == STORAGE_DIR]
         [last_added] = [i["hash"] for i in files if title in i["filename"]]
 
-        await message.edit_text(message.md_text + f"\n\n[[ipfs]](https://ipfs.io/ipfs/{last_added})", parse_mode="MarkdownV2")
+        await message.edit_text(message.md_text + f" [ipfs](https://ipfs.io/ipfs/{last_added})",
+                                parse_mode="MarkdownV2")
 
-        command = ["ipfs", "name", "publish", "--quieter", f"{dir_hash}"]
-
-        if K51_KEY:
-            logger.info(f"k51 key {K51_KEY}")
-            command.append(f"--key={K51_KEY}")
-
-        ipfs_publish = subprocess.run(command, capture_output=True)
-
-        if ipfs_publish.returncode:
-            logger.error(ipfs_publish.stderr.decode("utf-8"))
-            return
-
-        logger.info(f"ipns updated {ipfs_publish.stdout.decode('utf-8').rstrip()}")
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True, on_startup=startup)
